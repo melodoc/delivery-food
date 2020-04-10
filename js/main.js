360cefe09fba439cbf1da249e7ce6987ca7fff26
Liubov <liubov_rz@yahoo.com>
2020-04-10 15:05:41 +0300

    Add event for modal window

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal")
+const close = document.querySelector(".close")
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
