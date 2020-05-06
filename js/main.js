8dd742db2417df84aee61f86c73479380f2279a2
melodoc <l_tih@ro.ru>
2020-05-07 02:42:19 +0300

    Delete animation and small improvments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal")
-const close = document.querySelector(".close")
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
 function toggleModal() {
     modal.classList.toggle("is-open");
 }
-
-cartButton.addEventListener('click', toggleModal);
-close.addEventListener('click', toggleModal);
-
-new WOW().init();
